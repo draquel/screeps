9167a53131a4aff518fe117e914d4b7da3ba86d6
Dan Raquel <draquel@webjynx.com>
2023-01-25 21:59:24 -0500

    tweaked auto-respawn and memory clean bounderies to handle multiple rooms better.

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-const util = require('./util.root');
+//const util = require('./util.root');
 const rooms = require('./rooms');
 const creeps = require('./creeps');
 
 module.exports.loop = function () {
     //Cleanup Creep Memory
     function cleanupMemory(){
+        if(!Memory.creeps){ return; }
+
         if(Object.keys(Memory.creeps).length > Object.keys(Game.creeps).length){
             for(var name in Memory.creeps) {
                 if(!Game.creeps[name]) {
@@ -13,16 +15,17 @@ module.exports.loop = function () {
                         let memory = Memory.creeps[name];
                         console.log('Auto-Respawning: '+name+' in '+room.name);
                         rooms.pushSpawnQueue(room,{name:name, memory:memory});
+                    }else{
+                        console.log('Clearing memory:', name);
                     }
                     delete Memory.creeps[name];
-                    console.log('Clearing creep memory:', name);
                 }
             }
         }
     }
     cleanupMemory();
 
-    //Room Operations
+    //Room Ops
     var roomNames = Object.keys(Game.rooms);
     for(let i = 0; i < roomNames.length; i++){
         let room = Game.rooms[roomNames[i]];
@@ -32,7 +35,7 @@ module.exports.loop = function () {
         rooms.run(room);
     }
 
-    //Run Creep Roles
+    //Run Creep Ops
     for(let name in Game.creeps) {
         let creep = Game.creeps[name];
         creeps.run(creep);
